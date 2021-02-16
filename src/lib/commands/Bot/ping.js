b272064424725e26acce0a8454dd7e4206a2bd86
Ryan Salazar <ryansalazar0929@outlook.com>
2021-02-16 10:09:03 +0800

    Update ping.js

--- src/lib/commands/Bot/ping.js
+++ src/lib/commands/Bot/ping.js
@@ -3,23 +3,31 @@ const { MessageEmbed } = require('discord.js');
 const Command = require('../../structures/Command');
 
 module.exports = class PingCommand extends Command {
-  constructor(...args) {
-    super(...args, {
+  constructor(client, name) {
+    super(client, name, {
+      category: 'Bot',
       args: false,
       description: "Checks the bot's connection to the server.",
+      timing: 10,
     });
   }
 
   async run(message) {
     const WS_PING = this.client.ws.ping;
-    const CALCULATED_PING = (await message.createdTimestamp) - Date.now();
+    const CALCULATED_PING = Date.now() - (await message.createdTimestamp);
 
     const HYPIXEL_PING = await this.client.wrappers.hypixel.getPing();
 
     message.channel.send(
       new MessageEmbed()
         // eslint-disable-next-line
-        .setTitle('\✨ Ping')
+        .setTitle('Ping')
+        .setDescription(
+          [
+            `This is the status of the connection to Discord`,
+            `and other wrappers.`,
+          ].join('\n')
+        )
         .addFields(
           {
             name: 'Websocket Ping',
