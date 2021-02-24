7dd1c39893f3bfae42f72142b92a7c1d8dc31bb9
Ryan Salazar <30727670+RyanGamingXbox@users.noreply.github.com>
2021-02-24 09:31:29 +0800

    ✨ Extra fixes to RNG command.

--- src/lib/structures/commands/Fun/rng.ts
+++ src/lib/structures/commands/Fun/rng.ts
@@ -16,13 +16,18 @@ module.exports = class RngCommand extends BaseCommand {
   }
 
   // eslint-disable-next-line
-  async run( message: Message, args: string[] ) {
+  async run( message: Message, args: string[] ): Promise<void> {
     const minRNG: number = Number(args[0]);
     const maxRNG: number = Number(args[1]);
 
     const actualMax: number = maxRNG - minRNG;
 
-    const random = Math.floor( Math.random() * actualMax ) + 2 - minRNG;
+    if (maxRNG < minRNG) {
+      message.channel.send(`We can't handle a number from \`${minRNG}-${maxRNG}\`. What? The only thing in that range is \`undefined\`.`);
+      return;
+    };
+
+    const random: number = maxRNG === minRNG ? minRNG: Math.floor( Math.random() * actualMax ) + 1 + minRNG;
     message.channel.send(`The random number generated was \`${random}\`. The range you specified was \`${minRNG}-${maxRNG}\`.`);
   };
 }
