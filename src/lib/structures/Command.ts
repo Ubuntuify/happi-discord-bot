eae4bf224d226fe7565c4314674bb0d6e851d91d
Ryan Salazar <30727670+RyanGamingXbox@users.noreply.github.com>
2021-02-25 21:46:52 +0800

    🔨 Revamp Command.ts (might cause issues with the commands in place. I will rewrite them in the next commit.

--- src/lib/structures/Command.ts
+++ src/lib/structures/Command.ts
@@ -1,23 +1,22 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable no-unused-vars */
-/* eslint-disable class-methods-use-this */
-
-import { yellow } from 'chalk';
 import Discord from 'discord.js';
 import * as Client from '../../Client';
 
-interface CommandOptions {
-  category: string;
-  name: string;
+/**
+ * The options that defines what the commands are about.
+ */
+export interface CommandOptions {
+  category?: string;
+  name?: string;
+  usage?: string;
+  description?: string;
+  ownerOnly?: boolean;
+  timing: number;
   aliases: string[];
   args: boolean;
-  usage: string;
-  description: string;
-  timing: number;
-  ownerOnly: boolean;
 }
 
-export default abstract class Command {
+export default abstract class BaseCommand {
+  /* 📚 The properties of this class */
   client: Client.Interface;
   name: string;
   aliases: string[];
@@ -25,44 +24,32 @@ export default abstract class Command {
   args: boolean;
   usage: string;
   description: string;
-  timing: number;
+  cooldown: number;
   ownerOnly: boolean;
 
-  /**
-   * 📌 The main class for the commands.
-   *
-   * @param {Client} client The Client class instance initialized by index.
-   * @param {String} name The name and command trigger phrase.
-   * @param {Object} options The options for the command.
-   *
-   * @param {String} options.name The trigger phrase. (This will have a higher priority compared to the above name.)
-   * @param {String[]} options.aliases The shorter versions of this command.
-   *
-   * @param {Boolean} options.args Whether arguments exist for the command.
-   * @param {String} options.usage How to use this command if arguments exist.
-   * @param {String} options.description The command description.
-   * @param {Number} options.timing The timing of the command.
-   * @param {Boolean} options.ownerOnly If the command is exclusive to the owner.
-   */
-  constructor(client: Client.Interface, name: string, options: any) {
+  /* 🔨 The constructor for this class. */
+  constructor(client: Client.Interface, name: string, options: CommandOptions) {
+    /* ✨ Important parts of the command class. */
     this.client = client;
     this.name = options.name || name;
-    this.aliases = options.aliases || [];
 
-    this.category = options.category || 'Misc';
-    this.description = options.description || 'No description was specified.';
-
-    // This designates the options of the command.
+    /* 💫 Not essential but required components of this abstract class. */
+    this.aliases = options.aliases;
+    this.cooldown = options.timing;
     this.args = options.args;
-    this.usage = options.usage || 'Impossible. Perhaps the archives are incomplete.';
-    this.timing = options.timing;
+
+    /* 💤 Optional parts of this abstract class. */
+    this.category = options.category || 'Misc.';
+    this.description = options.description;
+    this.usage =
+      options.usage ||
+      'Usage guide was not provided. Contact the author for a fix.';
+
     this.ownerOnly = options.ownerOnly || false;
   }
 
-  async run(message: Discord.Message, args: string[]): Promise<void> {
-    throw new Error(
-      `${yellow('✖ ')} This run method was not implemented. See stacktrace for details.`
-    );
+  /* eslint-disable-next-line */
+  public async run(message: Discord.Message, args: string[]): Promise<void> {
+    throw new Error('This run method was not implemented successfully.');
   }
-
-};
+}
