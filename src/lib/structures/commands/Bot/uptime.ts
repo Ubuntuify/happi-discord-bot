4d68aba9c6c964261e349e8c0949f2dcf122b62e
Ryan Salazar <30727670+RyanGamingXbox@users.noreply.github.com>
2021-02-26 07:12:34 +0800

    ✨ Added uptime command.

---
+++ src/lib/structures/commands/Bot/uptime.ts
@@ -0,0 +1,25 @@
+/* eslint-disable class-methods-use-this */
+
+import { Message } from 'discord.js';
+import ms from '../../../../bin/functions/ms/ms';
+
+import BaseCommand from '../../Command';
+import * as Client from '../../../../Client';
+
+module.exports = class extends BaseCommand {
+  /* 💔 Passes options to main class. */
+  constructor(client: Client.Interface, name: string) {
+    super(client, name, {
+      timing: 10,
+      aliases: [],
+      args: false,
+    });
+  }
+
+  /* 📡 This runs when the command is run. */
+  public async run(message: Message, args: string[]): Promise<void> {
+    /* 💫 uptime code here. */
+    const uptime = ms(this.client.uptime, { long: true });
+    message.channel.send(`I've been running for ${uptime}!`);
+  }
+};
