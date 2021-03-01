c3eebcffeaf8c4b4822eefee467c65ef52095d81
Ryan Salazar <30727670+RyanGamingXbox@users.noreply.github.com>
2021-03-01 10:22:40 +0800

    💫 adding help.ts (not yet ready)

---
+++ src/lib/structures/commands/Info/help.ts
@@ -0,0 +1,37 @@
+/* eslint-disable class-methods-use-this */
+import { Message, MessageEmbed } from 'discord.js';
+import BaseCommand from '../../Command';
+
+import * as Client from '../../../../Client';
+
+module.exports = class extends BaseCommand {
+  constructor(client: Client.Interface, name: string) {
+    super(client, name, {
+      cooldown: 10,
+      aliases: [],
+      args: false,
+    });
+  }
+
+  public async run(message: Message, args: string[]): Promise<void> {
+    if (args[0]) {
+      /* 💫 checks and identifies with a code. */
+      const command =
+        this.client.commands.Commands.get(args[0]) ||
+        this.client.commands.Commands.get(
+          this.client.commands.Aliases.get(args[0])
+        );
+
+      if (command) {
+        this.generateCommandEmbed(message, command);
+      }
+    }
+  }
+
+  private async generateCommandEmbed(
+    message: Message,
+    command: BaseCommand
+  ): Promise<void> {
+    const messageSent = message.channel.send('\\💫 Fetching command values..');
+  }
+};
