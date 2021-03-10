b8fc608684c4ad69f5f44c86fa21c083ecbb08d9
Ryan Salazar <30727670+RyanGamingXbox@users.noreply.github.com>
2021-03-11 06:46:23 +0800

    💦 added options change.

--- src/lib/helpers/mongo.ts
+++ src/lib/helpers/mongo.ts
@@ -1,10 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 import { mongoPath } from '../../app/config/main_config.json';
 
-export default async (): Promise<any> => {
+export default async (): Promise<Mongoose> => {
   await mongoose.connect(mongoPath, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: true,
   });
   return mongoose;
 };
