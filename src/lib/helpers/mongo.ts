ba2d40339425a659b04e05c8e6899b1179bcef79
Ryan Salazar <30727670+RyanGamingXbox@users.noreply.github.com>
2021-03-11 10:12:00 +0800

    ✨ adds ReadyState to exports of mongo.ts

--- src/lib/helpers/mongo.ts
+++ src/lib/helpers/mongo.ts
@@ -1,6 +1,13 @@
 import mongoose, { Mongoose } from 'mongoose';
 import { mongoPath } from '../../app/config/main_config.json';
 
+export const ReadyState = {
+  0: 'Disconnected',
+  1: 'Connected',
+  2: 'Connecting',
+  3: 'Disconnecting',
+};
+
 export default async (): Promise<Mongoose> => {
   await mongoose.connect(mongoPath, {
     keepAlive: true,
@@ -8,5 +15,7 @@ export default async (): Promise<Mongoose> => {
     useUnifiedTopology: true,
     useFindAndModify: false,
   });
+  console.log(ReadyState[mongoose.connection.readyState]);
+
   return mongoose;
 };
